d74861fbbca843ace2618632b0bf7bb3fbf1b5f4
Bio <razakaalbiok@gmail.com>
2025-01-30 17:14:56 +0700

    update v.1

---
+++ bin/typesystem.dart
@@ -0,0 +1,20 @@
+/* type system atau sound type system adalah sistem logis yang terdiri 
+dari seperangkat aturan yang menetapkan properti atau tipe ke berbagai 
+konstruksi program komputer, seperti variabel, expression, fungsi, atau modul. 
+*/
+
+/*type system generic adalah tanda <>, contoh pada list digunakan untuk mengklasifikasi
+
+List<int> numberList = [1, 2, 3, 4, 5];
+List<String> stringList = ['Dart', 'Flutter', 'Android', 'iOS'];
+List dynamicList = [1, 2, 3, 'empat'];  // List<dynamic>
+
+tipe parameter generic dilambangkan E, T, K, atau V.
+
+contoh untuj mengurangi duplikasi code
+
+abstract class Cache<T> {
+  T getByKey(String key);
+  void setByKey(String key, T value);
+}
+*/  
